100ab622f6332fa0d2feff16c57364906dbe5119
Chris Marsh <cmhilfiker@gmail.com>
2020-08-06 09:42:26 +0000

    Add second navbar + css
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

---
+++ static/js/navbar.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+    
+    // Function to change the nav-bar on scroll
+    $(window).scroll(function(){
+        ($(window).scrollTop() >= 100) ? (
+            $('.fixed-nav-bar').addClass('scrolled'),
+            $('.the-bass').addClass('scrolled')
+        ) : (
+            $('.fixed-nav-bar').removeClass('scrolled'),
+            $('.the-bass').removeClass('scrolled')
+        );
+    });
+    
+    // Drop Down Function
+    $('#menuButton').on('change', function(){
+        ($('#menuButton').is(':checked')) ? (
+            $('.the-bass').addClass('dropped')
+        ) : (
+            $('.the-bass').removeClass('dropped')
+        );
+    });
+    
+});
+
+
+
+const app = (() => {
+  let body;
+  let menu;
+  let menuItems;
+
+  const init = () => {
+    body = document.querySelector('body');
+    menu = document.querySelector('.sub-menu-icon');
+    menuItems = document.querySelectorAll('.sub-nav__list-item');
+
+    applyListeners();
+  };
+
+  const applyListeners = () => {
+    menu.addEventListener('click', () => toggleClass(body, 'sub-nav-active'));
+  };
+
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass))
+    element.classList.remove(stringClass);else
+
+    element.classList.add(stringClass);
+  };
+
+  init();
+})();
