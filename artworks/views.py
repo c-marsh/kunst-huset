da0fa3638089c5c0fac57f38aa9bb6f08c1f4acb
Chris Marsh <cmhilfiker@gmail.com>
2020-08-10 14:55:36 +0000

    Add gallery view urls and media for art
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Artwork
 # Create your views here.
+
+
+def gallery(request):
+    # Shows galery view of all art
+    artworks = Artwork.objects.all()
+
+    context = {
+        'artworks': artworks,
+    }
+    return render(request, 'artworks/gallery.html', context)
--- kunst_huset/urls.py
+++ kunst_huset/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profiles.urls')),
-    # path('artworks/', include('artworks.urls')),
+    path('artworks/', include('artworks.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings. MEDIA_ROOT)
