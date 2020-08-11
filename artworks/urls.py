206357e4a0a7c2e0bade267dc2a4ad127664db07
Chris Marsh <cmhilfiker@gmail.com>
2020-08-11 15:18:37 +0000

    Add art details pages, paths and views
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- artworks/urls.py
+++ artworks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name='gallery'),
+    path('<art_id>', views.art_detail, name='art_detail'),
 ]
--- artworks/views.py
+++ artworks/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork
 # Create your views here.
 
 
 def gallery(request):
-    # Shows galery view of all art
+    # Shows gallery view of all art
     artworks = Artwork.objects.all()
 
     context = {
         'artworks': artworks,
     }
     return render(request, 'artworks/gallery.html', context)
+
+
+def art_detail(request, art_id):
+    # Shows detailed view of specific art piece
+    artwork = get_object_or_404(Artwork, pk=art_id)
+
+    context = {
+        'artwork': artwork,
+    }
+    return render(request, 'artworks/art_detail.html', context)
