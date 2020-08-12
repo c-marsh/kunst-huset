308c32545d8897459e6418b37bd425c4f43bdee6
Chris Marsh <cmhilfiker@gmail.com>
2020-08-12 12:16:13 +0000

    comment out code to ensure allauth functional in Development
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- kunst_huset/settings.py
+++ kunst_huset/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     # custom apps
     'home',
     'artworks',
-    'profiles'
+    'profiles',
+    'basket'
 
 ]
 
@@ -74,7 +75,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                # 'django.template.context_processors.request', Required by AllAuth
+                'django.template.context_processors.request', #Required by AllAuth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -152,16 +153,16 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 
 
 # Use SMTP account if not in development
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'kunst-huset@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
+# if 'DEVELOPMENT' in os.environ:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'kunst-huset@example.com'
+# else:
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#     EMAIL_USE_TLS = True
+#     EMAIL_PORT = 587
+#     EMAIL_HOST = 'smtp.gmail.com'
+#     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+#     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
 
 # Email verification/setup
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
