b7ee8979d996a6a1115cac20dc740fe6c392b259
Chris Marsh <cmhilfiker@gmail.com>
2020-08-14 00:53:16 +0000

    Checkout view completed
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- checkout/views.py
+++ checkout/views.py
@@ -19,60 +19,70 @@ def checkout(request):
     # import keys
     stripe_public_key = settings.STRIPE_PK
     stripe_secret_key = settings.STRIPE_SK
-    basket = request.session.get('basket', {})
 
-    # if request.method == 'POST':
-    #     basket = request.session.get('basket', {})
-    #     form_data = {
-    #         'full_name': request.POST['full_name'],
-    #         'email': request.POST['email'],
-    #         'phone_number': request.POST['phone_number'],
-    #         'country': request.POST['country'],
-    #         'postcode': request.POST['postcode'],
-    #         'town_or_city': request.POST['town_or_city'],
-    #         'street_address1': request.POST['street_address1'],
-    #         'street_address2': request.POST['street_address2'],
-    #         'county': request.POST['county'],
-    #     }
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
 
-    order_form = OrderForm()
-        # if order_form.is_valid():
-        #     order = order_form.save(commit=False)
-        #     pid = request.POST.get('client_secret').split('_secret')[0]
-        #     order.stripe_pid = pid
-        #     order.original_basket = json.dumps(basket)
-        #     order.save()
-        #     for item_id, item_data in basket.items():
-        #         try:
-        #             artwork = Artwork.objects.get(id=item_id)
-        #             if isinstance(item_data, int):
-        #                 order_line_item = OrderLineItem(
-        #                     order=order,
-        #                     artwork=artwork,
-        #                     quantity=item_data,
-        #                 )
-        #                 order_line_item.save()
-        #         except Artwork.DoesNotExist:
-        #             messages.error(request, (
-        #                 "One of the items in your basket wasn't found in our database. "
-        #                 "Please call us for assistance!")
-        #             )
-        #             order.delete()
-        #             return redirect(reverse('view_basket'))
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
 
-    # return message if bag is empty
-    if not basket:
-        messages.error(request, "There's nothing in your basket at the moment")
-        return redirect(reverse('gallery'))
+            for item_id, quantity in basket.items():
+                try:
+                    artwork = Artwork.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        item_id=item_id,
+                        quantity=quantity,
+                        artwork=artwork,
+                    )
+                    order_line_item.save()
+                # defensive design incase item isn't found, remove and redirect customer
+                except Artwork.DoesNotExist:
+                    messages.error(request, (
+                        "One of the items in your basket wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
 
-    current_basket = basket_content(request)
-    total = current_basket['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_completed', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+        basket = request.session.get('basket', {})
+        # return message if bag is empty
+        if not basket:
+            messages.error(
+                request, "There's nothing in your basket at the moment")
+            return redirect(reverse('gallery'))
+
+        current_basket = basket_content(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
     # warning in case public key not detected
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
