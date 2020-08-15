bfbbc8d7bc34009a22bd21b1dffe2313fdfbe940
Chris Marsh <cmhilfiker@gmail.com>
2020-08-15 17:42:29 +0000

    ⚠️ Pre-migration for Artist model/user connection rollback commit
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('<int:user_id>', views.edit_profile, name='edit_profile')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -5,23 +5,27 @@ from django.contrib import messages
 from .forms import UserForm, ArtistForm
 from .models import UserProfile
 
+from artworks.models import Artwork, Artists
+
 
 def profile(request):
     """ A view to individual profiles"""
     profile = get_object_or_404(UserProfile, user=request.user)
+    artworks = Artwork.objects.all()
 
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
+        'artworks': artworks
     }
 
     return render(request, template, context)
 
 
 @login_required
-def edit_profile(request):
+def edit_profile(request, user_id):
     """ Edit profiles"""
-    profile = get_object_or_404(UserProfile, user=request.user)
+    profile = get_object_or_404(UserProfile, user_id=request.user_id)
 
     template = 'profiles/edit_profile.html'
     context = {
