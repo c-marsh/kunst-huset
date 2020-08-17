e32a9e370debdfe4a9687a7fc0ad3fcebef76b9a
Chris Marsh <cmhilfiker@gmail.com>
2020-08-17 00:32:09 +0000

    Use webhook to update user form even if form submission fails
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 from .models import Order, OrderLineItem
 from artworks.models import Artwork
-# from profiles.models import UserProfile
+from profiles.models import UserProfile
 
 import json
 import time
@@ -35,6 +35,22 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+# Update profile information if save_info was checked
+        profile = None
+        username = request.user
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
 
         # check 5 times, 1 second apart if the order exists before creating
         # an instance from the webhook to prevent payment and no instance
