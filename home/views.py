98b40f41d70250fb290a7babaeb6b53354c5c1fe
Chris Marsh <cmhilfiker@gmail.com>
2020-08-19 07:42:45 +0000

    update home to show random profile
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import random
 from artworks.models import Category
+from profiles.models import UserProfile
 
 
 # Create your views here.
@@ -8,9 +10,12 @@ def index(request):
     Return homepage
     """
     categories = Category.objects.all()
+    items = UserProfile.objects.all()
 
+    random_item = random.choice(items)
     context = {
         'categories': categories,
+        'artist': random_item
     }
 
     return render(request, 'home/index.html', context)
