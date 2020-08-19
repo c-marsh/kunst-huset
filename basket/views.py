3210563a42cce17155f151fd251036fe10a03a29
Chris Marsh <cmhilfiker@gmail.com>
2020-08-19 11:14:23 +0000

    pep8
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- basket/views.py
+++ basket/views.py
@@ -27,7 +27,6 @@ def add_to_basket(request, item_id):
 
     basket = request.session.get('basket', {})
 
-
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
         messages.success(
