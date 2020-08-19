b88615a9cfb7d42c5b8545c77bc4e8cefe561c7d
Chris Marsh <cmhilfiker@gmail.com>
2020-08-19 01:49:32 +0000

    attempt to add stock control
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from artworks.models import Artwork
 from profiles.models import UserProfile
@@ -16,13 +17,25 @@ def basket_content(request):
 
     for item_id, quantity in basket.items():
         artwork = get_object_or_404(Artwork, pk=item_id)
+        # item_stock here should equal total available stock of item
         sub_total += quantity * artwork.price
+
         items_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'artwork': artwork,
-        })
+        if quantity <= item_stock:
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'artwork': artwork,
+            })
+        else:
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_stock,
+                'artwork': artwork,
+            })
+            messages.error(request,f"Sorry, there isn't enough stock to \
+                add {Artwork.title} to the basket.")
+
     if sub_total < settings.FIXED_DELIVERY_THRESHOLD:
         delivery = sub_total * Decimal(
             settings.STANDARD_DELIVERY_PERCENTAGE / 100)
